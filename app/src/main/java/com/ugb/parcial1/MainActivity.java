fe871f33dfd44317833e79277b6a80aa8afda8bb
eduardosantosugb <7edwardc7@gmail.com>
2025-03-24 10:18:00 -0600

    camara

--- app/src/main/java/com/ugb/parcial1/MainActivity.java
+++ app/src/main/java/com/ugb/parcial1/MainActivity.java
@@ -6,6 +6,10 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,58 +22,16 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tempVal;
-    SensorManager sensorManager;
-    Sensor sensor;
-    SensorEventListener sensorEventListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        sensorLuz();
-    }
-    @Override
-    protected void onResume() {
-        iniciar();
-        super.onResume();
-    }
-    @Override
-    protected void onPause() {
-        detener();
-        super.onPause();
-    }
-    private void iniciar(){
-        sensorManager.registerListener(sensorEventListener, sensor, 2000*1000);
-    }
-    private void detener(){
-        sensorManager.unregisterListener(sensorEventListener);
-    }
-    private void sensorLuz(){
-        tempVal = findViewById(R.id.lblSensorAcelerometro);
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        if( sensor==null ){
-            tempVal.setText("Tu dispositivo, NO tiene el senor de ACELEROMETRO");
-            finish();
-        }
-        sensorEventListener = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent event) {
-                double x = event.values[0];
-                double y = event.values[1];
-                double z = event.values[2];
-                tempVal.setText("Desplazamiento X= "+ x +"; Y= "+ y + "; Z= "+ z);
-            }
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-            }
-        };
     }
 }
